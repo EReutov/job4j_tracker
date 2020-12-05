b97a1d42a99dcdc950cff196be0ddcc2a812a97c
EReutov <ereutov30@gmail.com>
2020-12-05 19:12:22 +0500

    9. toString [#417750]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -7,10 +7,12 @@ public class StartUI {
     public static void main(String[] args) {
         /*Item item = new Item();
         System.out.println(item.getCreated().format(DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss")));
-        */
+
         Tracker tracker = new Tracker();
         tracker.add(new Item());
         System.out.println(tracker.findById(1));
+        */
 
+        System.out.println(new Item(1,"one"));
     }
 }
